9269ba512c175ec56da5bde5e8690124b6c3ac96
Olle K <olle@iix3.org>
2013-09-04 20:13:38 +0200

    Working on adding a list of items..

--- qpaste.py
+++ qpaste.py
@@ -3,18 +3,17 @@
 import os
 import tkinter
 
-class Popup(tkinter.Frame):
+class Popup(tkinter.Listbox):
 
     def __init__(self, master=None):
         tkinter.Frame.__init__(self, master)
+        master.title("qpaste")
+        for num, text in enumerate(self.refresh_data()):
+            self.insert(num+1, text)
         self.pack()
-        self.show_yourself()
-       
-    def show_yourself(self):
-        pass
 
     @staticmethod
-    def refresh_data(self):
+    def refresh_data():
         current_index = 0
         returnl = []
         with open("paste.cfg", encoding="utf-8") as f:
@@ -31,39 +30,15 @@ class Popup(tkinter.Frame):
         assert type(string_to_add) is str
         os.system("echo {} | clip".format(string_to_add.strip()))
 
-def complain(string):
-    print(string)
-
 def main():
     if os.name != "nt":
-        return complain(
+        return print(
             "Your OS is not recognized as Windows.\n" +
             "Running this on any other Operating System wont really work")
     stdscr = tkinter.Tk()
-    stdscr.title("qpaste")
-    testlabel = tkinter.Label(stdscr, text="Do some keypressing", width=30)
-    testlabel.pack()
-
-    def checkkey(event):
-        if event.char == event.keysym:
-            msg = 'Normal Key %r' % event.char
-        elif len(event.char) == 1:
-            msg = 'Punctuation Key %r (%r)' % (event.keysym, event.char)
-        else:
-            msg = 'Special Key %r' % event.keysym
-        testlabel.config(text=msg)
-    stdscr.bind_all("<Key>", checkkey)
-        
     pop = Popup(master=stdscr)
     #win.withdraw()
-    pop.mainloop()
-    """
-    lst = update_all()
-    num = input("Type number 1-{} to add it to clipboard: ".format(len(lst)))
-    if num.isdigit() and 1 <= int(num) <= len(lst):
-        add_to_clipboard(lst[int(num)-1])
-    """
-        
+    pop.mainloop()        
 
 if __name__ == "__main__":
     main()
