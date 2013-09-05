c3de61453be1a332ea6d3235efb351fbbaabf7e2
Olle Kvarnstrom <olle@iix3.org>
2013-09-05 14:52:10 +0200

    Made semi-working
    Now creates a checklist which adds the string to clipboard if theres no newline in the file

--- qpaste.py
+++ qpaste.py
@@ -8,22 +8,26 @@ class Popup(tkinter.Listbox):
     def __init__(self, master=None):
         tkinter.Listbox.__init__(self, master)
         master.title("qpaste")
-        for num, text in enumerate(self.refresh_data()):
-            self.insert(num+1, text)
+        self.data = None
+        for num, text in enumerate(self.refresh_data(), 1):
+            self.insert(num, text)
+        self.bind("<Double-Button-1>", lambda e:
+                  self.add_to_clipboard(self.refresh_data()[int(self.curselection()[0])]))
         self.pack()
 
-    @staticmethod
-    def refresh_data():
-        current_index = 0
-        returnl = []
-        with open("paste.cfg", encoding="utf-8") as f:
-            for line in f:
-                if line[:7] == "~split~":
-                    current_index += 1
-                    returnl.append("")
-                elif current_index:
-                    returnl[current_index-1] += line
-        return returnl
+    #@staticmethod
+    def refresh_data(self):
+        if self.data is None:
+            current_index = 0
+            self.data = []
+            with open("paste.cfg", encoding="utf-8") as f:
+                for line in f:
+                    if line[:7] == "~split~":
+                        current_index += 1
+                        self.data.append("")
+                    elif current_index:
+                        self.data[current_index-1] += line
+        return self.data
 
     @staticmethod
     def add_to_clipboard(string_to_add):
@@ -37,6 +41,7 @@ def main():
             "Running this on any other Operating System wont really work")
     stdscr = tkinter.Tk()
     pop = Popup(master=stdscr)
+    #pop.add_command(label="tst", command=main)
     #win.withdraw()
     pop.mainloop()        
 
